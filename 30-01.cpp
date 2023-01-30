1b43c85b5aeb9731eaee90ff953a05989dc8c0fc
lovishgupta1001 <lovishgupta1001@gmail.com>
2023-01-30 20:28:48 +0530

    30-01-2023 leetcode potd

---
+++ 30-01.cpp
@@ -0,0 +1,17 @@
+// Q-1137 https://leetcode.com/problems/n-th-tribonacci-number/
+class Solution
+{
+public:
+    int tribonacci(int n)
+    {
+        int ans[38] = {0};
+        ans[0] = 0;
+        ans[1] = 1;
+        ans[2] = 1;
+        for (int i = 3; i <= n; i++)
+        {
+            ans[i] = ans[i - 1] + ans[i - 2] + ans[i - 3];
+        }
+        return ans[n];
+    }
+};

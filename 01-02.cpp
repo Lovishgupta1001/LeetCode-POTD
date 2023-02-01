87f511ec051dd86b2fe01e0ff5a8f9feefb4732d
lovishgupta1001 <lovishgupta1001@gmail.com>
2023-02-01 18:56:49 +0530

    01-02-2023 leetcode potd

---
+++ 01-02.cpp
@@ -0,0 +1,9 @@
+// Q-1071 https://leetcode.com/problems/greatest-common-divisor-of-strings/
+class Solution
+{
+public:
+    string gcdOfStrings(string s1, string s2)
+    {
+        return (s1 + s2 == s2 + s1) ? s1.substr(0, gcd(s1.size(), s2.size())) : "";
+    }
+};
